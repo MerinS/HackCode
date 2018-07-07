a7306f8591ddcce9dc4eda04145ac092549f2047
dhatch2 <dhatch2@wisc.edu>
2018-07-07 06:32:05 -0700

    minor bug fix.

--- serial_interface.c
+++ serial_interface.c
@@ -12,10 +12,10 @@
 int main(int argc, char **argv)
 {
     if (argc != 2) {
-        printf("Usage: %s <Server IP>\n", argv[1]);
+        printf("Usage: %s <Server IP>\n", argv[0]);
         return -1;
     }
-    
+
     char *portname = "/dev/tty96B0";
     int fd;
     int wlen;
